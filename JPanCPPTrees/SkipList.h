f4819b3c2dceabe475bfa7a23f1740029f8daa94
Johnny Pan <Johnny.Pan@GMR.local>
2019-01-20 14:32:37 -0800

    fixed oorange, need to fix rng

--- JPanCPPTrees/SkipList.h
+++ JPanCPPTrees/SkipList.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <memory>
+#include <random>
+#include <time.h>
 #include "SkipListNode.h"
 using namespace std;
 
@@ -7,10 +9,10 @@ template <typename T>
 class SkipList {
 public:
 	int Count;
-	unique_ptr<SkipListNode<T>> head;
+	shared_ptr<SkipListNode<T>> head;
 
 	SkipList();
-	void ChooseRandHeight(unique_ptr<SkipListNode<T>>&);
+	void ChooseRandHeight(shared_ptr<SkipListNode<T>>&);
 	void Add(T);
 	bool Remove(T);
 	void Search(T);
@@ -23,31 +25,43 @@ SkipList<T>::SkipList() {
 }
 
 template <typename T>
-void SkipList<T>::ChooseRandHeight(unique_ptr<SkipListNode<T>>& node) {
-	while (rand() % 2 == 0 && node->Height <= head->Height + 1) {
+void SkipList<T>::ChooseRandHeight(shared_ptr<SkipListNode<T>>& node) {
+	std::mt19937 rng;
+	rng.seed(std::random_device()());
+	std::uniform_int_distribution<std::mt19937::result_type> dist2(0, 1);
+
+	while (dist2(rng) % 2 == 0 && node->Height < head->Height + 1) {
 		node->Height++;
 		node->Nexts.emplace_back(nullptr);
+	
+	if (node->Height > head->Height) {
+		for (int i = node->Height; i > head->Height; i--) {
+			head->Nexts.emplace_back(nullptr);
+		}
+		head->Height = node->Height;
 	}
 }
 
 template <typename T>
 void SkipList<T>::Add(T value) {
 	if (head == nullptr) {
-		head = make_unique<SkipListNode<T>>(value);
+		head = make_shared<SkipListNode<T>>();
 		return;
 	}
-	unique_ptr<SkipListNode<T>> newNode = make_unique<SkipListNode<T>>(value);
+	shared_ptr<SkipListNode<T>> newNode = make_shared<SkipListNode<T>>(value);
 	ChooseRandHeight(newNode);
-	if (newNode->Height > head->Height) {
-		head->Height = newNode->Height;
-	}
 	auto search = head.get();
 	int currHeight = head->Height - 1;
-	while (currHeight > -1 && (search->Nexts[currHeight] == nullptr || search->Nexts[currHeight]->Value > value)) {
-		if (currHeight < newNode->Height) {
-			newNode->Nexts[currHeight] = move(search->Nexts[currHeight]);
-			search->Nexts[currHeight] = move(newNode);
+	while (currHeight > -1) {
+		if (search->Nexts[currHeight] == nullptr || search->Nexts[currHeight]->Value > value) {
+			if (currHeight < newNode->Height) {
+				newNode->Nexts[currHeight] = search->Nexts[currHeight];
+				search->Nexts[currHeight] = newNode;
+			}
+			currHeight--;
 		}
-		currHeight--;
+		else if (search->Nexts[currHeight]->Value < value) {
+			search = search->Nexts[currHeight].get();
+		}
 	}
 }
--- JPanCPPTrees/SkipListNode.h
+++ JPanCPPTrees/SkipListNode.h
@@ -8,13 +8,20 @@ class SkipListNode {
 public:
 	T Value;
 	int Height;
-	vector<unique_ptr<SkipListNode<T>>> Nexts;
+	vector<shared_ptr<SkipListNode<T>>> Nexts;
 	SkipListNode(T);
+	SkipListNode();
 };
 
 template <typename T>
 SkipListNode<T>::SkipListNode(T value) {
 	Value = value;
+	Height = 1;
+	Nexts.emplace_back(nullptr);
+}
+
+template <typename T>
+SkipListNode<T>::SkipListNode() {
 	Height = 1;
 	Nexts.emplace_back(nullptr);
 }
